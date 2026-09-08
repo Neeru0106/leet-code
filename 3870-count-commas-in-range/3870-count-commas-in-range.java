30dd3c45ca17351d25ee5acf1da6c58eb4ef4f1a
PASUPULETI NEERAJ PAVAN <neerajpavan05@gmail.com>
2026-09-08 15:22:32 +0530

    Time: 0 ms (100%), Space: 42.4 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,22 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int count=0;
-        int digits=(int)Math.log10(n)+1;
-        if(digits>9){
-            count+=(n-999999999)*(digits-1)/3;
-            n=999999999;
-            digits=9;
-        } 
-        if(digits>6){
-            count+=(n-999999)*2;
-            n=999999;
-            digits=6;
+        if(n<1000){
+            return 0;
         }
-        if(digits>3){
-            count+=(n-999)*1;
-            n=999;
-            digits=3;
-        }
-        return count;
+        return n - 999;
     }
 }
