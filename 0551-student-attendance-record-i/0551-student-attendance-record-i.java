7c26c3191714dff59b25a96df30b4d68c47a6733
PASUPULETI NEERAJ PAVAN <neerajpavan05@gmail.com>
2026-09-17 12:28:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (22.35%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean checkRecord(String s) {
-        if(s.indexOf("LLL")>=0){
-            return false;
-        }
-        int a=0;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='A'){
-                a++;
+        int absent=0;
+        int late=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(ch=='A'){
+                absent++;
+                if(absent>=2){
+                    return false;
+                }
+                late=0;
+            }
+            else if(ch=='L'){
+                late++;
+                if(late>=3){
+                    return false;
+                }
+            }else{
+                late=0;
             }
         }
-        return a<2;
+        return true;
     }
 }
